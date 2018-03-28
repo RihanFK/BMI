d08d742b70db1e56667df6cc1552cb3843f24216
RihanFK <a20397185@gmail.com>
2018-03-28 15:45:01 +0800

    第一次

--- app/src/main/java/com/example/cjcu/bmi/MainActivity.java
+++ app/src/main/java/com/example/cjcu/bmi/MainActivity.java
@@ -38,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
         float bmi=w/(h*h);
         Log.d( "BMI=",String.valueOf(bmi));
         Toast.makeText(this,String.valueOf(bmi),Toast.LENGTH_LONG).show();
-        new AlertDialog.Builder(this).setMessage("BMI="+bmi) .setTitle("BMI值").setPositiveButton("確定",null)
-               .setPositiveButton("CANCEL",null) .show();
+
     }
 }
